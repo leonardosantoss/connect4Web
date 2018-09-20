73f5e837fd20b5cdf964f2080cc3c9ac7afb9233
Leonardo Santos <leonardosantos@Leonardos-MacBook-Pro.local>
2018-09-20 15:02:51 +0100

    Added a prototype button for the game instructions

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 var column = 7, row= 6; // make user determine the height and width
 
-
 // Generate the board, using a table
 var table = document.createElement("table");
 for (var i=0; i<row;i++){
@@ -22,3 +21,23 @@ var outsideDiv = document.createElement('div');
 outsideDiv.className = "connect4div";
 outsideDiv.appendChild(table);
 document.body.appendChild(outsideDiv);
+
+
+// button for the instructions option
+
+var modal = document.getElementById('myModal'); //modal
+var instructionsButton = document.getElementById('instructions'); //button
+var span = document.getElementsByClassName("close")[0]; // element that closes the modal
+
+// open the modal after user clicks on the button
+instructionsButton.onclick = function () {
+    modal.style.display = "block";
+}
+
+// closes the modal after clicking the x 
+span.onclick = function () {
+    modal.style.display = "none";
+}
+
+
+
