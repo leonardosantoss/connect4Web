4193aff64e45417861fbc2acbefcc227f202f2ae
Leonardo Santos <leonardosantos@Leonardos-MBP.lan>
2018-09-21 19:30:03 +0100

    Added generateBoard function

--- script.js
+++ script.js
@@ -1,27 +1,35 @@
 var column = 7, row= 6; // make user determine the height and width
 
-// Generate the board, using a table
-var table = document.createElement("table");
-for (var i=0; i<row;i++){
-    var tr = document.createElement('tr');
+function generateBoard(row, column){
     
-    for(var j=0; j<column ;j++){
-        var td = document.createElement('td');
-        var div = document.createElement('div');
-        var position = (i*row) + j + 1;   
-        td.className = "square"; 
-        div.className = "circle" + position;
-        td.appendChild(div);
-        tr.appendChild(td);
-        
-    }   
-    table.appendChild(tr);
+    // Generate the board, using a table
+    var table = document.createElement("table");
+    for (var i=0; i<row;i++){
+        var tr = document.createElement('tr');
+
+        for(var j=0; j<column ;j++){
+            var td = document.createElement('td');
+            var div = document.createElement('div');
+            var position = (i*row) + j + 1;   
+            td.className = "square"; 
+            div.className = "circle" + position;
+            td.appendChild(div);
+            tr.appendChild(td);
+
+        }   
+        table.appendChild(tr);
+    }
+
+    var outsideDiv = document.createElement('div');
+    outsideDiv.className = "connect4div";
+    outsideDiv.appendChild(table);
+    document.body.appendChild(outsideDiv);  
+
 }
 
-var outsideDiv = document.createElement('div');
-outsideDiv.className = "connect4div";
-outsideDiv.appendChild(table);
-document.body.appendChild(outsideDiv);
+generateBoard(row,column);
+
+
 
 
 // button for the instructions option
