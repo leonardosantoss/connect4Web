a524329f0ac39e20c7bdb88b04dca6dc0d443455
afonsonf <afonsofernandes@netcabo.pt>
2018-10-19 11:28:28 +0100

    change some var to const

--- script.js
+++ script.js
@@ -2,19 +2,19 @@ var gameMatrix, row, column, playerColor;
 
 // button for the instructions option
 
-var instructionsModal = document.getElementById('instructionsModal'); //modal
-var profileModal = document.getElementById('profileModal'); //modal
-var scoreModal = document.getElementById('scoreModal'); //modal
+const instructionsModal = document.getElementById('instructionsModal'); //modal
+const profileModal = document.getElementById('profileModal'); //modal
+const scoreModal = document.getElementById('scoreModal'); //modal
 
-var instructionsButton = document.getElementById('instructionsButton'); //button
-var profileButton = document.getElementById('profileButton'); // profile
-var scoreButton = document.getElementById('scoreButton'); //score
+const instructionsButton = document.getElementById('instructionsButton'); //button
+const profileButton = document.getElementById('profileButton'); // profile
+const scoreButton = document.getElementById('scoreButton'); //score
 
-var instructionsSpan = document.getElementById('instructionsSpan'); // element that closes the modal
-var profileSpan = document.getElementById('profileSpan'); // element that closes the modal
-var scoreSpan = document.getElementById('scoreSpan'); // element that closes the modal
+const instructionsSpan = document.getElementById('instructionsSpan'); // element that closes the modal
+const profileSpan = document.getElementById('profileSpan'); // element that closes the modal
+const scoreSpan = document.getElementById('scoreSpan'); // element that closes the modal
 
-var submitSelectors = document.getElementById('submitSelectors');
+const submitSelectors = document.getElementById('submitSelectors');
 
 // open the modal after user clicks on the button
 instructionsButton.onclick = function () {
@@ -46,17 +46,17 @@ scoreSpan.onclick = function () {
 
 // After clicking the submit button, handles all the selections made by the user
 submitSelectors.onclick = function (){
-    var against = document.getElementById('against');
-    var optionAgainst = against.options[against.selectedIndex].text;
+    const against = document.getElementById('against');
+    const optionAgainst = against.options[against.selectedIndex].text;
 
-    var sizeCol = document.getElementById('sizeCol').value;
+    const sizeCol = document.getElementById('sizeCol').value;
 
-    var sizeRow = document.getElementById('sizeRow').value;
+    const sizeRow = document.getElementById('sizeRow').value;
 
-    var ia = document.getElementById('ia').value;
+    const ia = document.getElementById('ia').value;
 
-    var whoStarts = document.getElementById('whoStarts');
-    var optionWhoStarts = whoStarts.options[whoStarts.selectedIndex].text;
+    const whoStarts = document.getElementById('whoStarts');
+    const optionWhoStarts = whoStarts.options[whoStarts.selectedIndex].text;
 
     playerColor = document.getElementById('color').value;
 
@@ -68,7 +68,7 @@ submitSelectors.onclick = function (){
     }
     else {
         //success, board can be safely generated
-        var table = document.getElementById('connect4div');    
+        const table = document.getElementById('connect4div');    
         if(table.innerHTML != null){
             table.innerHTML = "";
         }
