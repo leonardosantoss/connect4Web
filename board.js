48550c25db8a28ef5512f695b127bbbe61ff4085
Leonardo Santos <leonardosantos@Leonardos-MacBook-Pro.local>
2018-10-26 18:33:34 +0100

    Scoreboard

---
+++ board.js
@@ -0,0 +1,358 @@
+var player; // choose player that starts
+const unit_vector = [[1,0], [0,1], [-1,-1], [-1,1]]; // horizontal, vertical, negative diagonal, positive diagonal
+var bestplayCol;
+var vec_np;
+var fwin;
+
+function logmsg(currentCol, color, str){
+    const logDiv = document.getElementById('logDiv');
+    const p = document.createElement('p');
+    p.className = "logmsg";
+    p.innerHTML = "Move " + np +": " + str + " played on the column " + currentCol;
+    p.style.color = color;
+    logDiv.appendChild(p);
+}
+
+function update_board_pos(currentRow, currentCol){
+    const emptyPosition = (currentRow*column)+currentCol;
+    const circle = document.getElementById("cir_"+ emptyPosition);
+    
+    if(player){
+        play(currentRow,currentCol, "X");
+        circle.style.background = playerColor;
+        win = checkWin(currentRow, currentCol, "X");
+        if(againstBot) logmsg(currentCol, playerColor, "You");
+        else logmsg(currentCol, playerColor, "Player1");
+    }
+    else{
+        play(currentRow,currentCol, "O");
+        circle.style.background = opponentColor;
+        win = checkWin(currentRow, currentCol, "O");
+        if (againstBot) logmsg(currentCol, opponentColor, "Bot");
+        else logmsg(currentCol, opponentColor, "Player2");
+    }
+    
+    if(win){                        //WIN
+        setTimeout(function() {
+            alert("Win");
+        }, 0);
+
+        if(againstBot){
+            if(player){
+                addWin("Player1");
+                addLoss("Bot");
+            } 
+            else{
+                addWin("Bot");
+                addLoss("Player1");
+            }
+        }
+        else{
+            if(player){
+                addWin("Player1");
+                addLoss("Player2");
+            }
+            else{
+                addWin("Player2");
+                addLoss("Player1");
+            }
+        }
+        
+        return true;
+    }
+    else if(np == column*row){      //DRAW
+        setTimeout(function() {
+            alert("Draw");
+        }, 0);
+        if(againstBot){
+           addTie("Player1");
+           addTie("Bot");
+        }
+        else{
+            addTie("Player1");
+            addTie("Player2");
+        }
+    }
+    return false;
+}
+
+function playHuman(currentRow, currentCol) {
+    if(fwin) return;
+
+    const win = update_board_pos(currentRow, currentCol);                    
+    player = !player;
+    if(win) fwin = true;
+}
+
+function playBot(){
+    if(fwin) return;
+
+    const currentCol = alfabeta(difficulty);
+    const currentRow = findRow(currentCol);
+
+    const win = update_board_pos(currentRow, currentCol);                    
+    player = !player;
+    if(win) fwin = true;
+}
+
+function addTie(playerName){
+    let obj = score.find(obj => obj.name == playerName);
+    obj.ties++;
+}
+
+function addWin(playerName){
+    let obj = score.find(obj => obj.name == playerName);
+    obj.wins++;
+}
+
+function addLoss(playerName){
+    let obj = score.find(obj => obj.name == playerName);
+    obj.losses++;
+}
+
+function generateBoard(){
+    // Generate the board, using a table
+    const table = document.createElement("table");
+    table.className= "boardTable";
+    table.id="gameBoard";
+    for (let i=0; i<row;i++){
+        const tr = document.createElement('tr');
+        tr.className = "boardTr";
+        for(var j=0; j<column ;j++){
+            const td = document.createElement('td');
+            const div = document.createElement('div');
+            position = i*(column) + j;
+            td.className = "square";
+            td.id = position;
+            div.className = "circle";
+            div.id = "cir_" + position;
+
+            td.onmouseover = function (){
+                const currentCol = this.id % column;
+                for(let i=0;i<row;i++){
+                    const square = document.getElementById(i*column+currentCol);
+                    square.style.background = "lightgrey";
+                }
+            }
+            td.onmouseleave = function (){
+                const currentCol = this.id % column;
+                for(let i=0;i<row;i++){
+                    const square = document.getElementById(i*column+currentCol);
+                    square.style.background = "gray";
+                }
+            }
+            td.onclick = function (){ 
+                if((!player && againstBot) || fwin) return;
+
+                let currentCol = this.id % column;
+                let currentRow = findRow(currentCol);
+                
+                if(currentRow !== -1){
+                    playHuman(currentRow, currentCol);
+                    if(againstBot) playBot();
+                }
+                
+            }
+            
+            td.appendChild(div);
+            tr.appendChild(td);
+            
+
+        }   
+        table.appendChild(tr);
+    }
+
+    const outsideDiv = document.getElementById('connect4div');
+    outsideDiv.appendChild(table);
+
+}
+
+
+// returns a matrix with all positions with a "-" (means its empty)
+function createMatrix(){
+  let matrix = [];
+  for(let i=0;i<row;i++){
+      matrix[i] = [];
+      for(let j=0; j<column;j++){
+          
+          matrix[i][j] = "-";
+      }
+  }
+    return matrix;
+}
+
+function pos_gameMatrix(i, j){
+    return gameMatrix[i][j];
+}
+
+function update_gameMatrix(i, j, x){
+    return gameMatrix[i][j] = x;
+}
+
+// Player true represented by X on the gameMatrix
+// Player false represented by O on the gameMatrix
+// Function determines the correct position for the move to be played
+// in the correct column, based on where the player clicked
+// returns -1 in case the column  is full
+
+function findRow(currentCol){
+    if(vec_np[currentCol]==row) return -1;
+    return row-vec_np[currentCol]-1;
+    
+}
+
+
+function checkWin(currentRow, currentCol, typeToCheck){
+    if(currentRow == undefined || currentCol == undefined) return false;
+    let sum = 0;
+    let x;
+    
+    for(let i = 0;i<4;i++){
+        x = checkSum(currentRow, currentCol, typeToCheck, unit_vector[i]);
+        if(x > sum)
+            sum = x;
+    }
+    
+    //sum = Math.max(vertical(lastPlayPosition, typeToCheck), horizontal(lastPlayPosition, typeToCheck), diagonal_negative(lastPlayPosition,typeToCheck), diagonal_positive(lastPlayPosition,typeToCheck));
+
+    if(sum >= 3){
+        return true;
+    }
+    return false;
+}
+
+function checkSum(currentRow, currentCol, typeToCheck, vector){
+    let sum = 0;
+    let cond0, cond1;
+    
+    for(let i=1;i<4;i++){
+        cond0 = currentRow+i*vector[0] > row-1    || currentRow+i*vector[0] < 0;
+        cond1 = currentCol+i*vector[1] > column-1 || currentCol+i*vector[1] < 0;
+        if(cond0 || cond1){
+            break;
+        }
+        else if(pos_gameMatrix(currentRow+i*vector[0] , currentCol+i*vector[1]) !== typeToCheck){
+            break;
+        } 
+        else{
+            sum++;
+        }
+    }
+    
+    for(let i=1;i<4;i++){
+        cond0 = currentRow-i*vector[0] > row-1    || currentRow-i*vector[0] < 0;
+        cond1 = currentCol-i*vector[1] > column-1 || currentCol-i*vector[1] < 0;
+        if(cond0 || cond1){
+            break;
+        }
+        else if(pos_gameMatrix(currentRow-i*vector[0],currentCol-i*vector[1]) !== typeToCheck){
+            break;
+        } 
+        else{
+            sum++;
+        }
+    }
+    
+    //console.log(currentCol + " " + vector[0]+ "," + vector[1] + ": " + sum);
+    return sum;
+}
+
+function play(currentRow, currentCol, type){
+    //console.log(currentRow,currentCol);
+    np++;
+    vec_np[currentCol]++;
+    lastPlayCol = currentCol;
+    lastPlayRow = currentRow;
+
+   update_gameMatrix(currentRow,currentCol, type);
+}
+
+function rmplay(currentRow, currentCol, lastRow, lastCol){
+    np--;
+    vec_np[currentCol]--;
+    lastPlayCol = lastCol;
+    lastPlayRow = lastRow;
+    update_gameMatrix(currentRow,currentCol, "-");
+}
+
+function eval_pos_aux(currentRow, currentCol, vector){
+    let sum = 0;
+    let sum1 = 0;
+    let sum2 = 0;
+    let cond0, cond1;
+    let type1 = "";
+    let type2 = "";
+
+    for(let i=1;i<4;i++){
+        cond0 = currentRow+i*vector[0] > row-1    || currentRow+i*vector[0] < 0;
+        cond1 = currentCol+i*vector[1] > column-1 || currentCol+i*vector[1] < 0;
+        if(cond0 || cond1){
+            break;
+        }
+        else if(type1 == "" && pos_gameMatrix(currentRow+i*vector[0] , currentCol+i*vector[1]) !== "-"){
+            type1 = pos_gameMatrix(currentRow+i*vector[0] , currentCol+i*vector[1]);
+            sum1++;
+        }
+        else if(type1 != "" && pos_gameMatrix(currentRow+i*vector[0] , currentCol+i*vector[1]) !== type1){
+            break;
+        } 
+        else if(type1 != ""){
+            sum1++;
+        }
+    }
+    
+    for(let i=1;i<4;i++){
+        cond0 = currentRow-i*vector[0] > row-1    || currentRow-i*vector[0] < 0;
+        cond1 = currentCol-i*vector[1] > column-1 || currentCol-i*vector[1] < 0;
+        if(cond0 || cond1){
+            break;
+        }
+        else if(type2 == "" && pos_gameMatrix(currentRow-i*vector[0] , currentCol-i*vector[1]) !== "-"){
+            type2 = pos_gameMatrix(currentRow-i*vector[0] , currentCol-i*vector[1]);
+            sum2++;
+        }
+        else if(type2 != "" && pos_gameMatrix(currentRow-i*vector[0],currentCol-i*vector[1]) !== type2){
+            break;
+        } 
+        else if(type2 != ""){
+            sum2++;
+        }
+    }
+
+    if(type1 == "O") 
+        sum -= Math.pow(4, sum1);
+    else if(type1 == "X") 
+        sum += Math.pow(4, sum1);
+
+    if(type2 == "O") 
+        sum -= Math.pow(4, sum2);
+    else if(type2 == "X") 
+        sum += Math.pow(4, sum2);
+
+    return sum;
+}
+
+function eval_pos(currentRow, currentCol){
+    let sum = 0;
+    let x;
+    
+    for(let i = 0;i<4;i++){
+        x = eval_pos_aux(currentRow, currentCol, unit_vector[i]);
+        if(x > sum)
+            sum = x;
+    }
+    
+    return sum;
+}
+
+function eval_board(){
+    let sum = 0;
+
+    for(let currentCol = 0; currentCol<column; currentCol++){
+        const currentRow = findRow(currentCol);
+        if(findRow!=-1){
+            sum+=eval_pos(currentRow, currentCol);
+        }
+    }
+    return sum;
+}
---
+++ script.js
@@ -0,0 +1,272 @@
+var gameMatrix, row, column, playerColor, opponentColor, difficulty, againstBot;
+var score = [];
+
+
+// button for the instructions option
+window.onload = function (){
+    const instructionsModal = document.getElementById('instructionsModal'); //modal
+    const profileModal = document.getElementById('profileModal'); //modal
+    const scoreModal = document.getElementById('scoreModal'); //modal
+
+    const instructionsButton = document.getElementById('instructionsButton'); //button
+    const profileButton = document.getElementById('profileButton'); // profile
+    const scoreButton = document.getElementById('scoreButton'); //score
+    const giveUpButton = document.getElementById('giveup');
+
+    const instructionsSpan = document.getElementById('instructionsSpan'); // element that closes the modal
+    const profileSpan = document.getElementById('profileSpan'); // element that closes the modal
+    const scoreSpan = document.getElementById('scoreSpan'); // element that closes the modal
+
+    const submitSelectors = document.getElementById('submitSelectors');
+    const submitLogin = document.getElementById('submitLoginId');
+    const loginDiv = document.getElementById('loginDivId');
+    const nav = document.getElementById('nav');
+    const logout = document.getElementById('logout');
+    const allButHeaderDiv = document.getElementById('allButHeaderDivId');
+    
+    
+
+    // open the modal after user clicks on the button
+    instructionsButton.onclick = function () {
+        instructionsModal.style.display = "block";
+    }
+
+    profileButton.onclick = function () {
+        profileModal.style.display = "block";
+    }
+
+    scoreButton.onclick = function () {
+        displayScoreBoard();
+        scoreModal.style.display = "block";
+    }
+
+    giveUpButton.onclick = function () { 
+        if(againstBot){
+            addWin("Bot");
+            addLoss("Player1");
+        }
+        else{
+            addWin("Player2");
+            addLoss("Player1");
+        }
+    }
+
+    // closes the modal after clicking the x 
+    instructionsSpan.onclick = function () {
+        instructionsModal.style.display = "none";
+    }
+
+    // closes the modal after clicking the x 
+    profileSpan.onclick = function () {
+        profileModal.style.display = "none";
+    }
+
+    // closes the modal after clicking the x 
+    scoreSpan.onclick = function () {
+        scoreModal.style.display = "none";
+    }
+
+    // After clicking the submit button, handles all the selections made by the user
+    submitSelectors.onclick = function (){
+        submitconfig();
+    }
+
+    submitLogin.onclick = function (){
+        const username = document.getElementById('usernameLogin').value;
+        const password = document.getElementById('passwordLogin').value;
+        while(username === "" || password === ""){
+            // proper validation will be made when server gets implemented
+            // for now we will not accept only when fields are left empty 
+            alert("Username or password not valid. Fields can't be empty!");
+            username = document.getElementById('usernameLogin').value;
+            password = document.getElementById('passwordLogin').value;
+        }
+
+
+        document.getElementById('usernameLogin').value = "";
+        document.getElementById('passwordLogin').value = "";
+        allButHeaderDiv.style.display = "block"
+        loginDiv.style.display = "none";
+        nav.style.display = "block";
+        console.log(username, password);
+    }
+
+    logout.onclick = function() {
+        nav.style.display = "none";
+        loginDiv.style.display = "block";
+        allButHeaderDiv.style.display = "none";
+        console.log("User just logged out");
+    }
+
+    var logDiv = document.getElementById('logDiv');
+    logDiv.scrollTop = logDiv.scrollHeight; //always have the scroll down on the log of the plays
+
+    initScoreboard();
+    submitconfig();
+}
+
+function submitconfig(){
+    const against = document.getElementById('against');
+    const optionAgainst = against.options[against.selectedIndex].text;
+    const sizeCol = document.getElementById('sizeCol').value;
+    const sizeRow = document.getElementById('sizeRow').value;
+    const whoStarts = document.getElementById('whoStarts');
+    const optionWhoStarts = whoStarts.options[whoStarts.selectedIndex].text;
+
+    playerColor = document.getElementById('colorme').value;
+    opponentColor = document.getElementById('colorop').value;
+
+    const table = document.getElementById('connect4div');    
+    if(table.innerHTML != null){
+        table.innerHTML = "";
+    }
+
+    const logDiv = document.getElementById('logDiv');
+    if(logDiv.innerHTML != null){
+        logDiv.innerHTML = "";
+    }
+    np = 0;
+    row = Number(sizeRow);
+    column = Number(sizeCol);
+
+    if(row > 8 || row <4 || column > 8 || column < 4){
+        alert("Invalid game board size!\nRow size should be between 4 and 8. \nColumn size should be between 4 and 8.");
+        return;
+    }
+
+    vec_np = [];
+    for(let i = 0; i<column; i++) vec_np.push(0);
+    
+    generateBoard(); //row and column were strings, make sure to change to number
+    gameMatrix = createMatrix();
+    
+    fwin = false;
+
+    const size = column;
+    const dif = document.getElementById("ia").value;
+    sizetodepth(size, dif);
+
+    document.getElementById("thinking").style.display = "none";
+
+    if(optionAgainst == "Bot")
+        againstBot = true;
+    else againstBot = false;
+
+    if(optionWhoStarts == "Me"){
+        player = true;
+    }
+    else {
+        player = false;
+        if(againstBot) playBot();
+    }
+}
+
+function initScoreboard(){
+  score.push(new Score("Bot"));
+  score.push(new Score("Player1"));
+  score.push(new Score("Player2"));
+    
+}
+
+function Score(name) {
+    this.name = name;
+    this.wins = 0;
+    this.ties = 0;
+    this.losses = 0;
+}
+
+function addScoreboardTitles(title, tableRow) {
+    var td = document.createElement('td');
+    var p = document.createElement('p');
+    p.innerHTML = title;
+    p.className = "pScore";
+    td.appendChild(p);
+    tableRow.appendChild(td)
+}
+
+function displayScoreBoard() {
+    const scoreTable = document.getElementById('scoreTable');
+    if(scoreTable != null){
+        scoreTable.innerHTML = "";
+    }
+    var tr = document.createElement('tr');
+    addScoreboardTitles("Name", tr);
+    addScoreboardTitles("Wins", tr);
+    addScoreboardTitles("Ties", tr);
+    addScoreboardTitles("Losses", tr);
+    scoreTable.appendChild(tr);
+
+    for(var k in score){
+        tr = document.createElement('tr');
+        tr.className = "trScore";
+
+        var td = document.createElement('td');
+        td.className = "tdScore";
+        var p = document.createElement('p');
+        p.innerHTML = score[k].name;
+        td.appendChild(p);
+        tr.appendChild(td);
+        
+
+        td = document.createElement('td');
+        td.className = "tdScore";
+        p = document.createElement('p');
+        p.innerHTML = score[k].wins;
+        td.appendChild(p);
+        tr.appendChild(td);
+        
+
+        td = document.createElement('td');
+        td.className = "tdScore";
+        p = document.createElement('p');
+        p.innerHTML = score[k].ties;
+        td.appendChild(p);
+        tr.appendChild(td);
+
+
+        td = document.createElement('td');
+        td.className = "tdScore";
+        p = document.createElement('p');
+        p.innerHTML = score[k].losses;
+        td.appendChild(p);
+        tr.appendChild(td);
+
+        scoreTable.appendChild(tr);
+        
+    }
+}
+//(4) 2 5 10 14
+//(5) 2 5 10 12
+//(6) 2 5 09 10
+//(7) 2 5 08 09
+//(8) 2 5 07 09
+function sizetodepth(size, dif){
+    if(dif == "Easy"){
+        if(size == 4)      difficulty = 2;
+        else if(size == 5) difficulty = 2;
+        else if(size == 6) difficulty = 2;
+        else if(size == 7) difficulty = 2;
+        else               difficulty = 2;
+    }else if(dif == "Medium"){
+        if(size == 4)      difficulty = 5;
+        else if(size == 5) difficulty = 5;
+        else if(size == 6) difficulty = 5;
+        else if(size == 7) difficulty = 5;
+        else               difficulty = 5;
+    }else if(dif == "Hard"){
+        if(size == 4)      difficulty = 10;
+        else if(size == 5) difficulty = 10;
+        else if(size == 6) difficulty = 9;
+        else if(size == 7) difficulty = 8;
+        else               difficulty = 7;
+    }else if(dif == "Hardcore (may be slow)"){
+        if(size == 4)      difficulty = 14;
+        else if(size == 5) difficulty = 12;
+        else if(size == 6) difficulty = 10;
+        else if(size == 7) difficulty = 9;
+        else               difficulty = 9;
+    }
+}
+
+
+
